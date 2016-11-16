a14edb4d24404c3f110348b2339b790e0ee8c329
citralyy <nonaly.poespita@yahoo.com>
2016-11-16 14:01:40 +0700

    change host

--- src/host.js
+++ src/host.js
@@ -1,10 +1,11 @@
 module.exports = { 
-    core: "http://bateeq-core-api.mybluemix.net/v1/core",
+    core: "http://dl-core-api-uat-dev.mybluemix.net/v1/core",
+    auth: "http://dl-auth-api-uat-dev.mybluemix.net",
     //PUBLISH
-    inventory: "http://bateeq-inventory-api.mybluemix.net/v1/inventory",
-    merchandiser:"http://bateeq-inventory-api.mybluemix.net/v1/merchandiser",
-    master: "http://bateeq-pos-api.mybluemix.net/v1/master",
-    sales: "http://bateeq-pos-api.mybluemix.net/v1/sales"
+    inventory: "http://bateeq-inventory-api-dev.mybluemix.net/v1/inventory",
+    merchandiser:"http://bateeq-inventory-api-dev.mybluemix.net/v1/merchandiser",
+    master: "http://bateeq-pos-api-dev.mybluemix.net/v1/master",
+    sales: "http://bateeq-pos-api-dev.mybluemix.net/v1/sales"
     
     //DEV
     // inventory: "http://localhost:9070/v1/inventory",
