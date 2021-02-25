4563c294ef01c24cd5232a32f0ea3372cd978736
Joifarflex <Joifarjosuajosua@gmail.com>
2021-02-25 20:05:00 +0700

    Update struck print
    update struck print sales-cr and sales-retur-cr

--- src/modules/sales-return-cr/view.js
+++ src/modules/sales-return-cr/view.js
@@ -316,7 +316,7 @@ export class View {
         this.printStruk += "        <td colspan='3' class='text-left'> " + this.data.store.Address + " </td>";
         this.printStruk += "    </tr>";
         this.printStruk += "    <tr>";
-        this.printStruk += "        <td colspan='3' class='text-left'> " + this.data.store.Phone + " </td>";
+        this.printStruk += "        <td colspan='3' class='text-left'> Tlp. (0271) 719911</td>";
         this.printStruk += "    </tr>";
         this.printStruk += "    <tr>";
         this.printStruk += "        <td colspan='3' class='text-left'> No. NPWP: 31.579.110.3-532.000 </td>";
